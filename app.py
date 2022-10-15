7df50302a73e0ed591f4a1e53fb695ed64349548
Virgil <949508510@qq.com>
2022-10-15 10:23:52 +0800

    feat: 4 Impl

--- app.py
+++ app.py
@@ -8,12 +8,18 @@ app = Flask(__name__)
 
 # Connect to Database
 # 数据库中，Treasure 的class 0代表工具， 1代表配饰
+# 设置存储箱的最大存储量为 20
 
+max_storage = 20
+
+
+# Checked
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+# Checked
 @app.route('/login')
 def login():
     if request.method == 'GET':
@@ -21,22 +27,28 @@ def login():
         pwd = request.args.get('password')
         pwd = md5(pwd.encode('utf-8')).hexdigest()
         person = db['User'].find_one({"username": name})
-        if person == None:
+        if person is None:
             return render_template('sign.html')
         if person.get('pwd') == pwd:
-            reps = make_response(render_template('homepage.html',re=[name]))
+            reps = make_response(render_template('homepage.html', re=[name]))
             reps.set_cookie("username", name)
             return reps
 
+
+# Checked
 @app.route('/homepage')
 def homepage():
     re = [request.cookies.get("username")]
-    return render_template('homepage.html',re=re)
+    return render_template('homepage.html', re=re)
 
+
+# Checked
 @app.route('/sign')
 def sign():
     return render_template('sign.html')
 
+
+# Checked
 @app.route('/signup')
 def signup():
     if request.method == 'GET':
@@ -67,10 +79,12 @@ def signup():
                 "luck": init_luck,
                 "work": init_work
             })
-        reps = make_response(render_template('signup.html',name=username,tag=flag))
+        reps = make_response(render_template('signup.html', name=username, tag=flag))
         reps.set_cookie("username", username)
         return reps
 
+
+# Checked
 @app.route('/myinfo')
 def myinfo():
     username = request.cookies.get("username")
@@ -79,67 +93,157 @@ def myinfo():
     # row1为装备在身上的装备， row2为放在背包里的装备，装备的格式为：装备名称 等级 装备详情
     row1, row2 = [], []
     treasure = db["Treasure"]
-    for i in user.get("arm"):
-        if i == None:
-            break
-        item = treasure.find_one({"treasureId": i})
-        row1.append([item.get("name"), item.get("level")])
-    for i in user.get("backpack"):
-        if i == None:
-            break
-        item = treasure.find_one({"treasureId": i})
-        row2.append([item.get("name"), item.get("level"), i])
+    if len(user.get("arm")) > 0:
+        for i in user.get("arm"):
+            item = treasure.find_one({"treasureId": i})
+            row1.append([item.get("name"), item.get("level")])
+    if len(user.get("backpack")) > 0:
+        for i in user.get("backpack"):
+            item = treasure.find_one({"treasureId": i})
+            row2.append([item.get("name"), item.get("level"), i])
 
-    return render_template('myinfo.html',name=username,money=money,row1=row1,row2=row2)
+    return render_template('myinfo.html', name=username, money=money, row1=row1, row2=row2)
 
+
+# Checked
 @app.route('/equip_info')
 def equip_info():
-    return render_template('equipinfo.html')
+    if request.method == 'GET':
+        name = request.args.get('equipments')
+        treasure = db['Treasure']
+        item = treasure.find_one({'name': name})
+        equip_type, luck, ability = None, 0, 0
+        if item.get('class') == '0':
+            equip_type = "工具"
+            ability = item.get('property')
+        else:
+            equip_type = "配饰"
+            luck = item.get('property')
+        return render_template('equipinfo.html', name=name, id=item.get('treasureId'),
+                               equip_type=equip_type, luck=luck, ability=ability, quality=item.get('quality'))
 
+
+# Checked
 @app.route('/sto_info')
 def sto_info():
     if request.method == 'GET':
-        name = request.args.get('name')
-        treasure = db['Treasure'].find_one({"name": name})
-        cla = "工具类" if treasure.get('class') == 0 else "配饰类"
-        luck = 0 if treasure.get('class') == 0 else treasure.get('property')
-        ability = 0 if treasure.get('class') == 1 else treasure.get('property')
-        return render_template('stoinfo.html',
-                               name=name, treasureId=treasure.get('treasureId'),
-                               cla=cla, luck=luck, ability=ability)
+        name = request.args.get('storage')
+        treasure = db['Treasure']
+        item = treasure.find_one({'name': name})
+        equip_type, luck, ability = None, 0, 0
+        if item.get('class') == 0:
+            equip_type = "工具"
+            ability = item.get('property')
+        else:
+            equip_type = "配饰"
+            luck = item.get('property')
+        return render_template('stoinfo.html', name=name, id=item.get('treasureId'),
+                               equip_type=equip_type, luck=luck, ability=ability, quality=item.get('quality'))
 
+
+# Checked
 @app.route('/take_off')
 def take_off():
-    return render_template('take_off.html'  ,tag=0)
+    treasure = db['Treasure']
+    user = db['User']
+    tag = 0
+    item_name = request.args.get('equipments')
+    item = treasure.find_one({"name": item_name})
+    item_id = item.get("treasureId")
+    person = user.find_one({"username": request.cookies.get('username')})
+    armed = person.get("arm")
+    armed.remove(item_id)
+    backpack = person.get("backpack")
+    if backpack is None:
+        backpack = []
+    if len(backpack) == max_storage:
+        tag = 1
+        backpack_item = treasure.find({"treasureId": {"$in": backpack}},
+                                      {"quality": 1, "treasureId": 1}).sort("quality", pymongo.ASCENDING)
+        if backpack_item[0].get("quality") <= item.get("quality"):
+            backpack_item[0]["treasureId"] = item_id
+            backpack = [i.get("treasureId") for i in backpack_item]
+    else:
+        backpack.append(item_id)
+    if armed is None:
+        armed = []
+    user.update_one({"username": person.get("username")},
+                    {"$set": {"arm": armed, "backpack": backpack}})
+    return render_template('take_off.html', tag=tag)
 
+
+# Checked
 @app.route('/take_on')
 def take_on():
-    return render_template('take_on.html',tag=0)
+    user = db['User']
+    person = user.find_one({"username": request.cookies.get('username')})
+    tag = 0
+    treasure = db['Treasure']
+    item = treasure.find_one({"name": request.args.get('equipments')})
+    item_class = item.get('class')
+    armed = person.get('arm')
+    if len(armed) == 0:
+        armed = [item.get("treasureId")]
+        backpack = person.get('backpack')
+        backpack.remove(item.get('treasureId'))
+        user.update_one({"username": person.get("username")},
+                        {"$set": {"arm": armed, "backpack": backpack}})
+    elif 3 > len(armed):
+        cnt1, cnt2 = 0, 0
+        for i in armed:
+            if treasure.find_one({"treasureId": i}).get('class') == 0:
+                cnt1 += 1
+            else:
+                cnt2 += 1
+        if cnt1 == 1 and item_class == 0:
+            tag = 1
+        elif cnt2 == 2 and item_class == 1:
+            tag = 1
+        else:
+            item_id = item.get("treasureId")
+            armed.append(item_id)
+            backpack = person.get('backpack')
+            backpack.remove(item_id)
+            if backpack is None:
+                backpack = []
+            user.update_one({"username": request.cookies.get('username')},
+                            {"$set": {"backpack": backpack, "arm": armed}})
+    else:
+        tag = 1
+    return render_template('take_on.html', tag=tag)
+
 
 @app.route('/marinfo')
 def marinfo():
-
-    re=[['装备1',0,1,2,3],['装备2',0,1,2,3],['装备3',0,1,2,3]]
+    re = [['装备1', 0, 1, 2, 3], ['装备2', 0, 1, 2, 3], ['装备3', 0, 1, 2, 3]]
     return render_template('marinfo.html', re=re)
+
 
 @app.route('/buy')
 def buy():
     return render_template('buy.html', tag=0)
+
+
 @app.route('/sell')
 def sell():
     re = [['装备1', 'equip0001', 1, 2, 3], ['装备2', 'equip0002', 1, 2, 3], ['装备3', 'equip0002', 1, 2, 3]]
     return render_template('sell.html', re=re)
+
+
 @app.route('/sell_it')
 def sell_it():
     return render_template('sellinfo.html')
 
+
 @app.route('/look_for_treasure')
 def look_for_treasure():
-    return render_template('look_for_treasure.html',equip_name='手套',tag=0)
+    return render_template('look_for_treasure.html', equip_name='手套', tag=0)
+
 
 @app.route('/look_for_money')
 def look_for_money():
-    return render_template('look_for_money.html',money=10)
+    return render_template('look_for_money.html', money=10)
+
 
 app.config['DEBUG'] = True
 app.secret_key = '123456'
