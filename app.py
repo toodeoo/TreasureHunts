4e887315703b7bc78d4fd5327ccf45d709c1bf04
Virgil <949508510@qq.com>
2022-10-17 11:48:27 +0800

    feat: 3 Impl

--- app.py
+++ app.py
@@ -13,6 +13,20 @@ app = Flask(__name__)
 max_storage = 20
 
 
+def auto_dropout(person, itemId):
+    treasure = db['Treasure']
+    item = treasure.find_one({"treasureId": itemId})
+    backpack = person.get("backpack")
+    if backpack is None:
+        backpack = []
+    backpack_item = treasure.find({"treasureId": {"$in": backpack}},
+                                  {"quality": 1, "treasureId": 1}).sort("quality", pymongo.ASCENDING)
+    if backpack_item[0].get("quality") <= item.get("quality"):
+        backpack_item[0]["treasureId"] = item_id
+        backpack = [i.get("treasureId") for i in backpack_item]
+    return backpack
+
+
 # Checked
 @app.route('/')
 def index():
@@ -158,11 +172,7 @@ def take_off():
         backpack = []
     if len(backpack) == max_storage:
         tag = 1
-        backpack_item = treasure.find({"treasureId": {"$in": backpack}},
-                                      {"quality": 1, "treasureId": 1}).sort("quality", pymongo.ASCENDING)
-        if backpack_item[0].get("quality") <= item.get("quality"):
-            backpack_item[0]["treasureId"] = item_id
-            backpack = [i.get("treasureId") for i in backpack_item]
+        backpack = auto_dropout(person, item)
     else:
         backpack.append(item_id)
     if armed is None:
@@ -173,6 +183,7 @@ def take_off():
 
 
 # Checked
+# TODO: 如果物品是放在商场贩卖的话，我们不能装备
 @app.route('/take_on')
 def take_on():
     user = db['User']
@@ -215,23 +226,84 @@ def take_on():
 
 @app.route('/marinfo')
 def marinfo():
-    re = [['装备1', 0, 1, 2, 3], ['装备2', 0, 1, 2, 3], ['装备3', 0, 1, 2, 3]]
+    re = []
+    market_info = db['Market'].find({})
+    treasure = db['Treasure']
+    for i in market_info:
+        item = treasure.find_one({"treasureId": i.get("treasureId")})
+        luck, ability = 0, 0
+        if item.get('class') == 0:
+            ability = item.get('property')
+        else:
+            luck = item.get('property')
+        re.append([i.get('treasureId'), item.get('name'), item.get('quality'),
+                   luck, ability, i.get('price'), i.get('seller')])
     return render_template('marinfo.html', re=re)
 
 
 @app.route('/buy')
 def buy():
-    return render_template('buy.html', tag=0)
+    user = db['User']
+    market = db['Market']
+    tag = 0
+    item = market.find_one({"name": request.args.get("name")})
+    buyer = user.find_one({"username": request.cookies.get("username")})
+    seller = user.find_one({"username": item.get("seller")})
+
+    if buyer.get("username") == seller.get("username"):
+        return render_template('buy.html', tag=0)
+    if buyer.get("money") < item.get("price"):
+        tag = 2
+    else:
+        seller_backpack = seller.get("backpack")
+        seller_money = seller.get("money")
+        seller_backpack.remove(item.get("treasureId"))
+        seller_money += item.get('price')
+        user.update_one({"username": seller.get("username")},
+                        {"$set": {"backpack": seller_backpack, "money": seller_money}})
+
+        buyer_money = buyer.get("money")
+        buyer_money -= item.get("price")
+        if len(buyer.get("backpack")) < max_storage:
+            buyer_backpack = buyer.get("backpack")
+            buyer_backpack.append(item.get("treasureId"))
+        else:
+            tag = 1
+            buyer_backpack = auto_dropout(buyer, item.get("treasureId"))
+        user.update_one({"username": buyer.get("username")},
+                        {"$set": {"backpack": buyer_backpack, "money": buyer_money}})
+        market.delete_one({"treasureId": item.get("treasureId")})
+    return render_template('buy.html', tag=tag)
 
 
 @app.route('/sell')
 def sell():
-    re = [['装备1', 'equip0001', 1, 2, 3], ['装备2', 'equip0002', 1, 2, 3], ['装备3', 'equip0002', 1, 2, 3]]
+    re = []
+    user = db['User']
+    market = db['Market']
+    treasure = db['Treasure']
+
+    person = user.find_one({"username": request.args.get('username')})
+    backpack = person.get('backpack')
+    item_list = treasure.find({"treasureId": {"$in": backpack}})
+    for i in item_list:
+        luck, ability = 0, 0
+        if i.get('class') == 0:
+            ability = i.get("property")
+        else:
+            luck = i.get("property")
+        re.append([i.get("treasureId"), i.get("name"), i.get("quality"), luck, ability])
     return render_template('sell.html', re=re)
 
 
 @app.route('/sell_it')
 def sell_it():
+    price = request.args.get('price')
+    item_name = request.args.get('name')
+    treasure = db['Treasure']
+    market = db['Market']
+    item_id = treasure.find_one({"name": item_name}).get("treasureId")
+    market.insert_one({"treasureId": item_id, "price": price, "seller": request.cookies.get("username")})
     return render_template('sellinfo.html')
 
 
